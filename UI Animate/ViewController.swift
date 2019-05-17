318a851a1c2bee1b1518b8692d54b0476e478e31
Aland Sinduartha <alandsind@gmail.com>
2019-05-17 09:42:59 +0700

    add button to move the origin point using CENTER

--- UI Animate/ViewController.swift
+++ UI Animate/ViewController.swift
@@ -13,14 +13,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var viewAnimate: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        UIView.animate(withDuration: 5) {
-            self.viewAnimate.frame.origin.x = 150
-            self.viewAnimate.frame.origin.y = 200
-        }
-    
+          // Animate using Frame
+        
+//        print(self.viewAnimate.frame)
+//        UIView.animate(withDuration: 5) {
+//            self.viewAnimate.frame.origin.x = 150
+//            self.viewAnimate.frame.origin.y = 200
+//            print(self.viewAnimate.frame)
+//        }
     }
-
-
+    //move the origin to a defined point
+    @IBAction func animateButton(_ sender: UIButton) {
+        //defined point
+        let point:CGPoint = CGPoint(x: 200, y: 600.5)
+        UIView.animate(withDuration: 1.5) {
+            self.viewAnimate.center = point
+        }
+    }
+    //move the origin to center of the view
+    @IBAction func centerButton(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.5) {
+            self.viewAnimate.center = self.view.center
+        }
+    }
+    
 }
 
