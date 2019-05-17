b28f15612e8cb50ae5d59c77c376de698c2c3ae4
Aland Sinduartha <alandsind@gmail.com>
2019-05-17 08:54:57 +0700

    Initial Commit

---
+++ UI Animate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UI Animate
+//
+//  Created by Aland Sinduartha on 17/05/19.
+//  Copyright © 2019 Aland Sinduartha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
